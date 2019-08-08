5d8c71cbb66ab2202940f1f21e495c4011a1ebf5
litianlin <1052023708@qq.com>
2019-08-08 20:41:45 +0800

    2019-8-8 合并前提交

--- src/main/java/com/bonc/config/RabbitConfig.java
+++ src/main/java/com/bonc/config/RabbitConfig.java
@@ -1,8 +1,11 @@
 package com.bonc.config;
 
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,20 +19,30 @@ import org.springframework.context.annotation.Scope;
  * @Description TODO
  */
 @Configuration
+@EnableRabbit
 public class RabbitConfig {
-//	@Bean(initMethod="afterPropertiesSet",destroyMethod="destroy")
-//	public ConnectionFactory rabbitConnectionFactory() {
-//		CachingConnectionFactory ccf=new CachingConnectionFactory(host,port);
-//		ccf.setPassword(password);
-//		ccf.setUsername(username);
-//		ccf.setHost(host);
-//		return ccf;
-//	}
+	@Bean(initMethod="afterPropertiesSet",destroyMethod="destroy")
+	public ConnectionFactory rabbitConnectionFactory() {
+		//封装RabbitConnectionFactory，复用连接
+		CachingConnectionFactory ccf=new CachingConnectionFactory(host,port);
+		ccf.setPassword(password);
+		ccf.setUsername(username);
+		ccf.setHost(host);
+		return ccf;
+	}
+	//消息操作模板
 	@Bean(initMethod="afterPropertiesSet")
 	@Scope("SCOPE_PROTOTYPE")
 	public AmqpTemplate amqpTemplate(ConnectionFactory cf) {
-		RabbitTemplate rt=new RabbitTemplate(cf);		
+		RabbitTemplate rt=new RabbitTemplate(cf);
 		return rt;
+	}
+	//操作exchange、binding、queue
+	@Bean(initMethod="afterPropertiesSet")
+	@Scope("SCOPE_PROTOTYPE")
+	public AmqpAdmin amqpAdmin(ConnectionFactory cf) {
+		AmqpAdmin aa=new RabbitAdmin(cf);	
+		return aa;
 	}
 	
 }
