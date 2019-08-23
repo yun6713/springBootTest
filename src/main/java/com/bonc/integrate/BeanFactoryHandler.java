4deeff83a9f2a8e9fd7b2e377e8d492073062cc3
litianlin <1052023708@qq.com>
2019-08-23 20:25:01 +0800

    2019-8-23 20:24:56

--- src/main/java/com/bonc/integrate/BeanFactoryHandler.java
+++ src/main/java/com/bonc/integrate/BeanFactoryHandler.java
@@ -1,5 +1,7 @@
 package com.bonc.integrate;
 
+import java.util.Properties;
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
@@ -8,14 +10,19 @@ import org.springframework.beans.factory.config.PropertyOverrideConfigurer;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.env.PropertySourceLoader;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertyResolver;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.PropertySources;
+import org.springframework.core.env.PropertySourcesPropertyResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.SystemPropertyUtils;
+
 /**
  * 操作bean Configuration metadata<br/>
  * 获取bean操作会跳过BeanPostProcessor<br/>
@@ -37,17 +44,19 @@ public class BeanFactoryHandler implements BeanFactoryPostProcessor {
 	PropertySource i;
 	PropertySources j;
 	PropertySourceLoader k;
-	
-	
-	
+	PropertySourcesPropertyResolver l;
+	ImportBeanDefinitionRegistrar m;
+	SystemPropertyUtils s;
 	ObjectUtils o;
-	
-	
-	
-	
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		
+		ConfigurableEnvironment env = beanFactory.getBean(ConfigurableEnvironment.class);
+		System.out.println(env.getProperty("test"));
+		System.out.println(env.getProperty("spring.mail.username"));
+		MutablePropertySources ps=env.getPropertySources();
+		ps.get("applicationConfig: [classpath:/application.yml]");
+		System.out.println(env.getProperty("test"));
+				
 	}
 
 }
